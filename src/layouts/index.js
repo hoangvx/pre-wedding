cc5342ccb962e041a7215ed139d17cd6d1ba6d4e
hoang.vx <hoang.vx@glv.co.jp>
2019-06-27 09:48:13 +0900

    update auto play audio

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,17 +3,25 @@ import React from "react";
 import NavBar from "../components/navbar";
 import Footer from "../components/footer";
 
+import cnd from './cuoi_nhau_di.mp3';
+
 class Layout extends React.Component {
 
   componentDidMount() {
-    var audio = new Audio('./cuoi_nhau_di.mp3');
-    audio.play();
+    // this.audio.play();
   }
 
   render() {
     let { children } = this.props
     return (
       <div>
+        <audio
+          ref={audio => this.audio = audio}
+          className="ht-audio"
+          controls autoPlay={true}>
+          <source src={cnd} type="audio/mp3" />
+          Your browser does not support the audio element.
+        </audio>
         <NavBar />
         {children}
         <Footer />
