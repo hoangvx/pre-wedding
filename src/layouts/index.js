55e99347189838210473dd7c76b123803765d026
hoang.vx <hoang.vx@glv.co.jp>
2019-06-27 12:05:58 +0900

    change audio player

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -6,12 +6,18 @@ import Footer from "../components/footer";
 import cnd from './cuoi_nhau_di.mp3';
 
 class Layout extends React.Component {
-
   render() {
     let { children } = this.props
     return (
       <div>
-        <iframe title="audio" className="ht-audio" src={cnd} allow="autoplay" id="iframeAudio"/>
+        {/* <iframe title="audio" className="ht-audio" src={cnd} allow="autoplay" id="iframeAudio"/> */}
+        <audio
+          ref={audio => this.audio = audio}
+          id="audio"
+          autoPlay={true}>
+          <source src={cnd} type="audio/mp3" />
+          Your browser does not support the audio element.
+        </audio>
         <NavBar />
         {children}
         <Footer />
